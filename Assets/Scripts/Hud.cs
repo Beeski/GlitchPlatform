7e2178f89de86d272c59e65debb4a64060b0b3d4
Lizi Attwood <lizi@beeski.com>
2015-02-21 23:12:36 +0000

    First version, scene, basic code and some prefabs. Glitch sprites including the Beeski character Idles, Walk and Jump - all hooked up with an Animator.

---
+++ Assets/Scripts/Hud.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hud : MonoBehaviour 
+{
+	[SerializeField] private SpriteRenderer [] Lives;
+	[SerializeField] private TextMesh Score;
+
+	// Update is called once per frame
+	void Update() 
+	{
+	
+	}
+}
